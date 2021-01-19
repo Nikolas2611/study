2c17b07ff6279aa79eb4d1131426bfdf4067ab83
Nikolas2611 <umrihin2611@gmail.com>
2021-01-19 10:48:49 +0200

    Названия фильмов

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numberOfFilms = +prompt('Скольок фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt('На сколько оцените его?', ''),
+      d = prompt('Один из последних просмотренных фильмов', ''),
+      c = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[d] = c;
+
+console.log(personalMovieDB);
